59ff3f6837aa1443c995b6ad830f03dc587a6f79
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-07-17 13:08:13 +0300

    initial

---
+++ data.js
@@ -0,0 +1,281 @@
+const jsonExample =
+  [
+    {
+      "id": 3240,
+      "name": "1",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3241,
+      "name": "2",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3242,
+      "name": "3",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3243,
+      "name": "4",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3244,
+      "name": "5",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3245,
+      "name": "6",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3246,
+      "name": "7",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3247,
+      "name": "8",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3248,
+      "name": "9",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3249,
+      "name": "10",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3250,
+      "name": "11",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3340,
+      "name": "12",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3440,
+      "name": "13",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3540,
+      "name": "14",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3640,
+      "name": "15",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3740,
+      "name": "16",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3840,
+      "name": "17",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3940,
+      "name": "18",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 4240,
+      "name": "19",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 5240,
+      "name": "29",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 6240,
+      "name": "28",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 7240,
+      "name": "27",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 8240,
+      "name": "26",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 9240,
+      "name": "25",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 140,
+      "name": "24",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 240,
+      "name": "23",
+      "description": null,
+      "url": "http://www.youtube.com/embed/mPhboJR0Llc",
+      "mediatype": 2,
+      "likeCount": 0,
+      "place": null,
+      "createAt": "0001-01-01T00:00:00"
+    }];
+const dataInfo = jsonExample;
+const contentEl = document.querySelector('.content');
+dataInfo.forEach(element => {
+  const divEl = document.createElement('div');
+  const imgEl = document.createElement('img');
+  const h2El = document.createElement('h2');
+  const pEl = document.createElement('p');
+  divEl.style.border = '3px solid black';
+  divEl.style.margin = '16px';
+  divEl.style.padding = '16px';
+  imgEl.setAttribute('alt', 'photo');
+  imgEl.src = element.url;
+  h2El.textContent = element.id;
+  pEl.textContent = element.name;
+  contentEl.appendChild(divEl);
+  divEl.appendChild(imgEl);
+  divEl.appendChild(h2El);
+  divEl.appendChild(pEl);
+});
